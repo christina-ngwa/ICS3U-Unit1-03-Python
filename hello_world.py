0a7482f163d3ae20944ae9a6431e316a0897b7a2
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-09-12 18:46:20 +0000

    commit message

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Christina Ngwa
+# Created on : September 2019
+# This is the "Hello, World!", but with nice style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
